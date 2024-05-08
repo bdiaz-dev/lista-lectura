325f41058266a34a7b499f7c769fd4b5374c9ead
Bruno Diaz <brunodiaz.dev@gmail.com>
2024-05-08 11:43:13 +0200

    Update BrunoDiazSign.jsx
    link to portfolio target blank

--- src/components/BrunoDiazSign.jsx
+++ src/components/BrunoDiazSign.jsx
@@ -3,7 +3,7 @@ import '../styles/BrunoDiazSign.css'
 export function BrunoDiazSign () {
   return (
     <div className='BrunoDiazSign'>
-      <a href='https://brunodiaz.es'>
+      <a href='https://brunodiaz.es' target='blank'>
         <b>By BrunoDiaz.es</b>
       </a>
     </div>
